bbf8424ae5e0cb9d656aac271d68d7db1b94ff63
Dennis Campagna <denniscampagna@gmail.com>
2016-07-01 01:07:26 +0200

    Final Update (si spera)

--- create_env.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-    putenv("OPENSHIFT_MYSQL_DB_HOST=127.0.0.1");
-    putenv("OPENSHIFT_MYSQL_DB_PORT=3306");
-    putenv("OPENSHIFT_MYSQL_DB_USERNAME=adminDEpA86H");
-    putenv("OPENSHIFT_MYSQL_DB_PASSWORD=kjrxMims8u4i");
-    putenv("OPENSHIFT_APP_NAME=timwebapp");
-
-?>
